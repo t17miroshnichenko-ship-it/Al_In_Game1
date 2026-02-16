4da9175b1361a0f2915f1f253867ead61c64c607
miros <t17miroshnichenko@gmail.com>
2026-02-16 15:38:24 +0200

    U4

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        self.image = pygame.image.load("ship.bmp")
+        self.image = pygame.image.load("ship.png")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
@@ -16,8 +16,8 @@ class Ship:
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
